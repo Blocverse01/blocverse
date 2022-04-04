ea7f065954b277e4f36417561bc2e1fb5740e1c3
TC.eth <67268931+KingzRex@users.noreply.github.com>
2022-04-04 15:33:08 +0100

    Update projects.js

--- src/store/projects.js
+++ src/store/projects.js
@@ -11,7 +11,7 @@ const projects = [
   {
     title: "Bloc-MetaverseV1",
     description:
-      "This is a metaverse project where users get to purchase lands in the Metavere, this project is open for contribution and is openly deployed to the Binance Smart chain. ",
+      "This is a metaverse project where users get to purchase lands on the Metaverse, this project is open for contribution and is openly deployed to the Binance Smart chain Testnet. ",
     imageSrc: Blocmeta,
     projectLink: "https://blocmeta.on.fleek.co/",
   },  
