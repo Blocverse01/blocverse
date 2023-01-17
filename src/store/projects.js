946534898e1b163437225feb40c13a805611a720
KingzRex <toochukwukingz6@gmail.com>
2023-01-17 10:51:41 +0000

    update uwazi on projects

--- src/store/projects.js
+++ src/store/projects.js
@@ -2,7 +2,15 @@ import blocticks from "projects/Tixhive_banner.png";
 import Blocmeta from "projects/Blocmeta.PNG";
 import web3gram from "projects/web3gram_landing.png";
 import avc from "projects/avc.png";
+import uwazi from "projects/uwazi-banner.png";
 const projects = [
+  {
+    title: "Uwazi",
+    description:
+      "An open fund raising system for wildlife conservancies. Uwazi empowers conservancies with a platform to make their financial needs for specific problems visible to interested donors around the world as funding requests, providing access to funds in a transparent yet secure manner, powered by blockchain technology. Uwazi is the financial powerhouse for making conservation a responsibility for everyone.",
+    imageSrc: uwazi,
+    projectLink: "https://tixhive.com",
+  },
   {
     title: "TixHive",
     description:
